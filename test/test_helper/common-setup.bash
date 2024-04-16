34bd2a84c035e5d684b56ff5ab1da864d5dd289c
Balazs Varnai <balazs.varnai@siemens.com>
2024-04-16 08:54:04 +0200

    Start server

--- test/test_helper/common-setup.bash
+++ test/test_helper/common-setup.bash
@@ -12,6 +12,9 @@ _common_setup() {
     # as those will point to the bats executable's location or the preprocessed file respectively
     DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
 
+    #export BATS_FILE_TMPDIR=~/bats-tmp
+    #mkdir -p $BATS_FILE_TMPDIR
+
     # start configuration server
     static-web-server/$SERVER_EXECUTABLE --directory-listing --port 8787 --root ./conf &
     echo $! > $BATS_FILE_TMPDIR/.test-server.pid
